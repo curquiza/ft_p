94c66ba4ee135b61e85468ba4c27726d2a9b3aae
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-08-23 14:39:17 +0200

    Minor changes

--- ftp_server/src/exec_type_cmd.c
+++ ftp_server/src/exec_type_cmd.c
@@ -3,5 +3,5 @@
 void		exec_type_cmd(t_user *user, char *cmd)
 {
 	(void)cmd;
-	send_oneline_reply_to_user(user, "200 Type set to: Binary.");
+	send_oneline_reply_to_user(user, "200 New type set.");
 }
