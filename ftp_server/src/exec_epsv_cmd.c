0e262b6694821c117b7dfc6b558abfc782d6e78d
Clementine Urquizar <clementine@meilisearch.com>
2019-09-16 16:15:07 +0200

    EPSV command done

--- ftp_server/src/exec_epsv_cmd.c
+++ ftp_server/src/exec_epsv_cmd.c
@@ -1,6 +1,6 @@
 #include "server.h"
 
-static void		send_pasv_response(t_user *user, int addr)
+static void		send_epsv_response(t_user *user, int addr)
 {
 	char		*response;
 	char		*tmp_int;
@@ -16,17 +16,13 @@ static void		send_pasv_response(t_user *user, int addr)
 
 static t_ex_ret		get_user_dt_port(t_user *user, int sock)
 {
-	struct sockaddr_in	sin;
 	uint16_t			port;
 
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = htonl(DEFAULT_SIN_ADDR);
 	port = PORT_MIN_RANGE;
 	while (port <= PORT_MAX_RANGE)
 	{
-		sin.sin_port = htons(port);
 		print_debug_output("Testing port", port, "for DT channel...", NULL);
-		if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)) != -1)
+		if (bind_server(sock, port) == SUCCESS)
 		{
 			user->dt_port = port;
 			print_debug_output("Available port found :", port, NULL, NULL);
@@ -37,17 +33,32 @@ static t_ex_ret		get_user_dt_port(t_user *user, int sock)
 	return (FAILURE);
 }
 
-static int		create_server_socket_on_valid_port(t_user *user)
+static int	socket_according_to_af(void)
 {
 	int					sock;
 	struct protoent		*proto;
 
+	sock = -1;
 	if ((proto = getprotobyname(TCP_PROTONAME)) == NULL)
+		return (ret_err_neg(PROTOBYNAME_ERR));
+	if (g_addr_family == AF_INET6)
 	{
-		print_debug_output(NULL, 0, PROTOBYNAME_ERR, NULL);
-		return (-1);
+		if ((sock = socket(PF_INET6, SOCK_STREAM, proto->p_proto)) == -1)
+			return (ret_err_neg(SOCKET_ERR));
 	}
-	if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
+	else
+	{
+		if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
+			return (ret_err_neg(SOCKET_ERR));
+	}
+	return (sock);
+}
+
+static int		create_server_socket_on_valid_port(t_user *user)
+{
+	int					sock;
+
+	if ((sock = socket_according_to_af()) == -1)
 	{
 		print_debug_output(NULL, 0, SOCKET_ERR, NULL);
 		return (-1);
@@ -65,11 +76,29 @@ static int		create_server_socket_on_valid_port(t_user *user)
 	return (sock);
 }
 
-void		exec_epsv_cmd(t_user *user, char *cmd)
+static t_ex_ret	accept_according_to_af(t_user *user)
 {
 	unsigned int		dt_size;
 	struct sockaddr_in	dt_sin;
+	struct sockaddr_in6	dt_sin6;
 
+	if (g_addr_family == AF_INET6)
+	{
+		if ((user->dt_client_sock = accept(user->dt_server_sock,
+			(struct sockaddr *)&dt_sin6, &dt_size)) < 0)
+			return (FAILURE);
+	}
+	else
+	{
+		if ((user->dt_client_sock = accept(user->dt_server_sock,
+			(struct sockaddr *)&dt_sin, &dt_size)) < 0)
+			return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+void		exec_epsv_cmd(t_user *user, char *cmd)
+{
 	if (cmd_has_no_arg(user, cmd) == FALSE)
 		return ;
 	if ((user->dt_server_sock = create_server_socket_on_valid_port(user)) == -1)
@@ -79,9 +108,8 @@ void		exec_epsv_cmd(t_user *user, char *cmd)
 	}
 	print_data_output("Socket server listening on port", user->dt_port,
 		NULL, NULL);
-	send_pasv_response(user, DEFAULT_SIN_ADDR);
-	if ((user->dt_client_sock = accept(user->dt_server_sock,
-		(struct sockaddr *)&dt_sin, &dt_size)) < 0)
+	send_epsv_response(user, DEFAULT_SIN_ADDR);
+	if (accept_according_to_af(user) == FAILURE)
 	{
 		print_debug_output(NULL, 0, ACCEPT_ERR, NULL);
 		send_oneline_reply_to_user(user, RES_425);
