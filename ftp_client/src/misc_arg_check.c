15a6b9f25fa8435002358a8e94a75039e47e2962
Clementine Urquizar <clementine@meilisearch.com>
2019-10-26 20:15:13 +0200

    Misc in separated files

--- ftp_client/src/misc_arg_check.c
+++ ftp_client/src/misc_arg_check.c
@@ -1,24 +1,5 @@
 #include "client.h"
 
-void	usage(char *prgm)
-{
-	ft_printf("Usage: %s: ./client [-6] <address> <port>\n", prgm);
-	ft_printf("  -6 : IPv6\n");
-	ft_printf("  -a : force active mode\n");
-}
-
-t_ex_ret		print_and_return_failure(char *str)
-{
-	ft_printf("%s\n", str);
-	return (FAILURE);
-}
-
-int				print_and_return_neg(char *str)
-{
-	ft_printf("%s\n", str);
-	return (-1);
-}
-
 t_bool	has_no_arg(char *input)
 {
 	char	**args_tab;
